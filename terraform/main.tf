6276e581872f0bb48704016700e6f2edf80a5a0a
vaishnavimurali <48913700+vaishnavimurali@users.noreply.github.com>
2023-03-06 11:30:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,7 @@
 provider "aws" {
   region                  = "${var.region}"
   access_key = "AKIAWA3S5VZA4HU6JLPM"
-  secret_key = "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
-"
+  secret_key = "9ubPJCo0yBApcpAWDLOBN/o4q3Iw2A6TKAur62fk"
   
   # shared_credentials_file = "~/.aws/credentials"
   # profile                 = "development"
